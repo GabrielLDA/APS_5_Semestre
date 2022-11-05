905e2d4ee4d0be00c6c9a4be471e4f9de7bf2021
GabrielLDA <gabriellima__2018@hotmail.com>
2022-11-05 13:53:08 -0300

    Image Edit

--- src/Client/Login.java
+++ src/Client/Login.java
@@ -37,8 +37,9 @@ public class Login extends JFrame {
         this.getContentPane().setBackground(Color.WHITE);
         
         jl_title.setBounds(10, 10, 375, 100);
-        ImageIcon icon = new ImageIcon("D:\\APS5Semestre\\APS5Semestre\\src\\Logo.png");
-        jl_title.setIcon(new ImageIcon(icon.getImage().getScaledInstance(375, 100, Image.SCALE_SMOOTH)));
+        ImageIcon icon = new ImageIcon("C:\\Users\\Administrador\\Desktop\\APS5Semestre\\APS5Semestre\\src\\Logo.png");
+        jl_title.setIcon(new ImageIcon(icon.getImage().getScaledInstance(150, 100, Image.SCALE_SMOOTH)));
+        jl_title.setHorizontalAlignment(SwingConstants.CENTER);
         
         jb_login.setBounds(10, 220, 375, 40);
         
